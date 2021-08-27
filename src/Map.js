5b3c96d34e6e2a457573c7c3c3c505ba215f1b0d
Shubham <shubham998845@gmail.com>
2021-08-27 17:14:01 +0530

    map leaflet working

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { MapContainer as LeafletMap, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import './Map.css';
 import { showDataOnMap } from './util';
 
 function Map({ countries, casesType, center, zoom }) {
   return (
     <div className="map">
-      <LeafletMap center={center} zoom={zoom}>
+      <LeafletMap center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
